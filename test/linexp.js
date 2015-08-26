4b6be9b818493fa2319d055d341ed1695233eec2
mohayonao <mohayonao@gmail.com>
2015-08-27 06:44:18 +0900

    add linexp(value: number, inMin, inMax, outMin, outMax): number

---
+++ test/linexp.js
@@ -0,0 +1,16 @@
+var assert = require("power-assert");
+var linexp = require("../linexp");
+
+function closeTo(expected, actual, delta) {
+  return Math.abs(expected - actual) <= delta;
+}
+
+describe("linexp(value: number, inMin, inMax, outMin, outMax): number", function() {
+  it("works", function() {
+    assert(closeTo(linexp(0.1, 0.01, 1, 100, 8000), 148.93891324884, 1e-6));
+    assert(closeTo(linexp(0.3, 0.01, 1, 100, 8000), 360.96974634425, 1e-6));
+    assert(closeTo(linexp(0.5, 0.01, 1, 100, 8000), 874.84966106965, 1e-6));
+    assert(closeTo(linexp(0.7, 0.01, 1, 100, 8000), 2120.2938396499, 1e-6));
+    assert(closeTo(linexp(0.9, 0.01, 1, 100, 8000), 5138.7640259933, 1e-6));
+  });
+});
