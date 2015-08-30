c560d89c292358cb4acc657bf5dd8e694f8c0079
mohayonao <mohayonao@gmail.com>
2015-08-30 13:29:09 +0900

    fix test in istanbul

--- test/noop.js
+++ test/noop.js
@@ -1,10 +1,14 @@
 var assert = require("power-assert");
 var noop = require("../noop");
+var inIstanbul = Object.keys(global).some(function(x) { return /^\$\$cov_/.test(x); });
 
 describe("noop(): void", function() {
   it("works", function() {
     var functionBody = noop.toString().replace(/\s/g, "");
 
-    assert(functionBody === "function(){}");
+    if (!inIstanbul) {
+      assert(functionBody === "function(){}");
+    }
+    assert.doesNotThrow(noop);
   });
 });
